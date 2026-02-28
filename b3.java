a3201326b7e1977e041a0b067290daccc78a233d
XLM <khoado.devie@gmail.com>
2026-02-28 22:09:18 +0700

    Add files via upload

---
+++ b3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số nguyên N: ");
+        int n = sc.nextInt();
+
+        n = Math.abs(n);
+
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Tổng các chữ số: " + sum);
+
+        sc.close();
+    }
+}
